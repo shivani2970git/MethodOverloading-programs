cedf64e0f47e2f05a81a808ee5baf89ed91c4037
Shivani Nikam <nikamshivani965@gmail.com>
2024-12-02 11:12:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+ static int add(int a,int b) {
+	 return a+b;
+	 }
+ static int add(int a,int b,int c) {
+	 return a+b+c;
+ }
+ public static void main(String args[]) {
+	 System.out.println(Main.add(20,10));
+	 System.out.println(Main.add(20,10,30));
+ }
+	
+
+}
